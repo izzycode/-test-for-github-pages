98a9689de81db284e89040350329ec3dad6c6b42
Israel Martinez <marcemc@yahoo.com>
2020-04-16 19:40:05 -0400

    Build for github pages

---
+++ precache-manifest.b2cf52ea341f9893e1c3378c70e96835.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80e3f834b65e5e4f6b531710d9075bef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57a0f65f03237705ca11",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bea1c9cce0b94f0c617b",
+    "url": "/static/js/2.f12017ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f12017ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57a0f65f03237705ca11",
+    "url": "/static/js/main.38426182.chunk.js"
+  },
+  {
+    "revision": "e2c3bcce230a8e3f5d95",
+    "url": "/static/js/runtime-main.f7ae5bd1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
